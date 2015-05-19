f4f0f5d489ed4da2ce5cfe4f163c9663ac3f0295
Jayin Ton <tonjayin@gmail.com>
2015-05-19 21:20:14 +0800

    observe the data change

--- src/main.js
+++ src/main.js
@@ -1,16 +1,38 @@
 var BASE_URL = 'http://localhost:3000/api/v1'
 
-var getWebsites = function() {
-    fetch(BASE_URL + '/websites')
+var wa_tag = document.getElementsByTagName('script')[0]
+var website_id = wa_tag.getAttribute('id')
+
+var addEvent = function(website_id, category, action, opt_label, opt_value) {
+
+    fetch(BASE_URL + '/website/:website_id/events'.replace(':website_id', website_id), {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                category: category,
+                action: action,
+                opt_label: opt_label,
+                opt_value: opt_value
+
+            })
+        })
         .then(function(response) {
             return response.json()
         })
         .then(function(json) {
-            console.log(json)
+            if (json.msg){
+                console.error(json.msg)
+            }
         })
 }
 
-
-getWebsites()
-
-console.log('it work!')
+Object.observe(_wa, function(changes) {
+    var c = changes[0]
+    if (c.type == 'add') {
+        var obj = _wa[_wa.length - 1]
+        addEvent(website_id, obj.category, obj.action, obj.opt_label, obj.opt_value)
+    }
+})
